492ff42f35d2be8127f93aeb4f0bb2c2b167adf2
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2025-02-14 12:03:40 +0000

    Add pointer_to_x method.
    Also makes read take &mut self, like the docs say it does.

--- examples/basic.rs
+++ examples/basic.rs
@@ -11,8 +11,6 @@ struct Uart {
 }
 
 fn main() {
-    println!("Hello, world!");
-
     let mut uart = Uart {
         data: 0xA,
         control: 0xC,
@@ -21,11 +19,14 @@ fn main() {
 
     // Safety: We're pointing at a real object
     let mut mmio_uart = unsafe { Uart::new_mmio(core::ptr::addr_of_mut!(uart)) };
+    println!("sample UART is @ {:p}", core::ptr::addr_of_mut!(uart));
 
     println!("data = {}", mmio_uart.read_data());
+    println!("data register is at = {:p}", mmio_uart.pointer_to_data());
     mmio_uart.modify_control(|f| {
         println!("control was {f}, is now 32");
         32
     });
     println!("control = {}", mmio_uart.read_control());
+    println!("control register is @ {:p}", mmio_uart.pointer_to_control());
 }
