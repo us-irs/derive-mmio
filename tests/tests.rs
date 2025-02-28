6cd361f4aecc3eb151a86ec72a4ebc0778db9f4d
Jonathan Pallant <jonathan.pallant@ferrous-systems.com>
2025-02-28 12:12:29 +0000

    Add test for double borrows.

--- tests/tests.rs
+++ tests/tests.rs
@@ -2,13 +2,14 @@
 fn all_tests() {
     let t = trybuild::TestCases::new();
     t.pass("tests/cases/basic.rs");
-    t.pass("tests/cases/no_ctors.rs");
     t.pass("tests/cases/inner_mmio.rs");
+    t.pass("tests/cases/no_ctors.rs");
 
-    t.compile_fail("tests/cases/cant_fake_inner_block.rs");
-    t.compile_fail("tests/cases/repr_c_mandatory.rs");
-    t.compile_fail("tests/cases/padding_forbidden.rs");
-    t.compile_fail("tests/cases/read_only.rs");
     t.compile_fail("tests/cases/bad_inner_attr.rs");
     t.compile_fail("tests/cases/bad_outer_attr.rs");
+    t.compile_fail("tests/cases/cant_fake_inner_block.rs");
+    t.compile_fail("tests/cases/inner_mmio_double_borrow.rs");
+    t.compile_fail("tests/cases/padding_forbidden.rs");
+    t.compile_fail("tests/cases/read_only.rs");
+    t.compile_fail("tests/cases/repr_c_mandatory.rs");
 }
