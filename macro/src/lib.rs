cd716e48769aeefcee365327fd9986c650b37540
Robin Mueller <robin.mueller.m@gmail.com>
2025-03-25 15:12:55 +0100

    add ptr getter methods

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -144,6 +144,13 @@ pub fn derive_mmio(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
                 }
             }
 
+            /// Retrieve the base pointer for this MMIO handle.
+            #[inline]
+            #[rustversion::since(1.74.1)]
+            pub const unsafe fn ptr(&self) -> *mut #ident {
+                self.ptr
+            }
+
             #access_methods_quoted
         }
 
